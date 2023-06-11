a28d3c838cdae7b83e77e6aaf9feff4b9737b4b6
Mohin Patel <mpate156@stevens.edu>
2023-06-11 22:26:11 +0000

    Added my own information into the links and title.

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mohin Patel",
+  title: "CS & Quantitative Finance Student",
+  email: "mohinpatel1728@gmail.com",
+  gitHub: "https://github.com/mohinpatell",
+  instagram: "https://www.instagram.com/_mohin_patel_/",
+  linkedIn: "https://www.linkedin.com/in/mohin-patel/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://twitter.com/the_mohin_patel",
 };
 
 const primaryColor = "#4E567E";

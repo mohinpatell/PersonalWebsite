3bbb03b2054e88f6615cb14135027fd027b7bfca
Mohin Patel <mpate156@stevens.edu>
2023-06-11 22:41:09 +0000

    More changes to my information.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,20 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a Computer Science and Quantitative Finance student at Stevens Institute of Technology. I am passionate about solving problems in new creative ways to drive innovation. By leveraging my OpenAI experience I continually look to make AI accessible in new ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Fullstack development",
+  "Azure cloud services",
+  "AWS cloud services",
+  "Bloomberg Terminal",
+  "OpenAI API",
+  "Responsive Web Design",
+  "Algorithms and Data Structures",
 ];
 
 /**
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "''The best way to predict the future is to create it.'' - Abraham Lincoln";
 
 const About = () => {
   return (

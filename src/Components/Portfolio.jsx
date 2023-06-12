3dfde01bde421585903158b0d97953b1ee2214fd
Mohin Patel <mpate156@stevens.edu>
2023-06-12 03:14:30 +0000

    final color

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -54,7 +54,7 @@ const projectList = [
 const Portfolio = () => {
   return (
     <section className="padding" id="portfolio">
-      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+      <h5 style={{ textAlign: "center" }}>Portfolio</h5>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
@@ -67,7 +67,7 @@ const Portfolio = () => {
           {projectList.map((project) => (
             <div className="box" key={project.title}>
               <a href={project.url} target="_blank" rel="noopener noreferrer">
-                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+                <h4 style={{ flexBasis: "40px" }}>{project.title}</h4>
               </a>
               <p className="small">{project.description}</p>
             </div>
